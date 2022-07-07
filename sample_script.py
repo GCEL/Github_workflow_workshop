a57330358827354b058c1b33a33f0c467bbca853
David Milodowski <DTMilodowski@users.noreply.github.com>
2022-07-07 11:28:32 +0100

    command line argument for plotting colour (#4)
    This closes issue #3

--- sample_script.py
+++ sample_script.py
@@ -2,14 +2,25 @@
 A sample script for the GCEL Github workshop, July 2022
 -------------------------------------------------------
 """
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as col
+
+if len(sys.argv)>1:
+    c = sys.argv[1]
+else:
+    c = 'seagreen'
+
+if not col.is_color_like(c):
+    print('warning: supplied colour not recognised, switching to default')
+    c = 'seagreen'
 
 x = np.arange(-4,4,0.01)*np.pi
 y = np.sin(x)*np.cos(x**1.5)**2
 
 fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(8,4))
-ax.plot(x,y,'-',color='seagreen',linestyle='--')
+ax.plot(x,y,'-',color=c,linestyle='--')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 fig.tight_layout()
