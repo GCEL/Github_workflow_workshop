709d39f0fe27836c19e9cd1d5781af51e1dd3d8f
David Milodowski <d.t.milodowski@ed.ac.uk>
2022-07-07 10:53:05 +0100

    decreasing step in x

--- sample_script.py
+++ sample_script.py
@@ -4,7 +4,7 @@ A sample script for the GCEL Github workshop, July 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-4,4,0.1)*np.pi
+x = np.arange(-4,4,0.01)*np.pi
 y = np.sin(x)*np.cos(x)**2
 
 fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(6,4))
