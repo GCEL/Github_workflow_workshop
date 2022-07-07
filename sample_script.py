71baadd34489eb2386daf2b6e12cc88975685584
David Milodowski <DTMilodowski@users.noreply.github.com>
2022-07-07 11:23:35 +0100

    Update sample_script.py
    Some minor changes to generate a conflict

--- sample_script.py
+++ sample_script.py
@@ -1,5 +1,6 @@
 """
 A sample script for the GCEL Github workshop, July 2022
+-------------------------------------------------------
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +9,10 @@ x = np.arange(-4,4,0.01)*np.pi
 y = np.sin(x)*np.cos(x**1.5)**2
 
 fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(8,4))
-ax.plot(x,y,'-',color='seagreen')
-fig.savefig('test.png')
+ax.plot(x,y,'-',color='seagreen',linestyle='--')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+fig.tight_layout()
+fig.savefig('updated_test.png')
 
 
