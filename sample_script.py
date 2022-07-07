9f1763d40cc1bd906459450fb4e8ad2de6f13656
David Milodowski <d.t.milodowski@ed.ac.uk>
2022-07-07 10:56:46 +0100

    saving the figure

--- sample_script.py
+++ sample_script.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 x = np.arange(-4,4,0.01)*np.pi
 y = np.sin(x)*np.cos(x)**2
 
-fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(6,4))
+fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(8,4))
 ax.plot(x,y,'-',color='seagreen')
-fig.show()
+fig.savefig('test.png')
+
 
